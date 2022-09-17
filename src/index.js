65828b875729df46c47ddd34f2e9ac7c6d645b65
flpdorea <felipeldorea@gmail.com>
2022-09-17 17:31:31 -0300

    Added edit todo functionality

--- src/index.js
+++ src/index.js
@@ -70,7 +70,16 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body
+  const { id } = request.params
+  const { user } = request
+
+  const todo = user.todos.find(item => item.id === id)
+
+  todo.title = title
+  todo.deadline = new Date(deadline)
+
+  return response.json(todo)
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
